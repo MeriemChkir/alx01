9d293ba09826ecda61d095d28f15734f91d220c7
chkirmeriem <chkirmeriem619@gmail.com>
2022-04-07 18:12:09 -0700

    5th c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - First function executed
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - First function executed
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
