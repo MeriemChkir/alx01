d9f577ecb8ca34882d19fc26530f36bca5d96eef
chkirmeriem <chkirmeriem619@gmail.com>
2022-04-07 18:39:33 -0700

    7th c commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ * main - First function executed 
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char x;
+	short y;
+	int z;
+	long int a;
+	long long b;
+	printf("Size of a char: %d byte(s)", sizeof(x));
+	printf("Size of a short: %d byte(s)", sizeof(y));
+	printf("Size of a char: %d byte(s)", sizeof(z));
+	printf("Size of a char: %d byte(s)", sizeof(a));
+	printf("Size of a char: %d byte(s)", sizeof(b));
+	return (0); 
