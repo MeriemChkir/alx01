34aef626fc15be0c0f99ba7b5ce11d3e8564b5e3
chkirmeriem <chkirmeriem619@gmail.com>
2022-04-11 09:55:29 -0700

    third commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - First Function
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (i = 'A'; i <= 'Z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
