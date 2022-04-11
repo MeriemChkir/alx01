edf92b13c731c0418b3565691248751b10054ec9
chkirmeriem <chkirmeriem619@gmail.com>
2022-04-11 09:06:11 -0700

    third commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-	{	putchar(i);
-		putchar('\n'); }
+	for (i = 'a'; i <= 'z'; i++){
+		putchar(i); }
+		putchar('\n'); 
 	return (0); 
 }  
