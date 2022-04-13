1e1e46fd41c23edbfe97abddf9e7509bc4c92eab
chkirmeriem <chkirmeriem619@gmail.com>
2022-04-13 08:06:07 -0700

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*main - Check description
+*Description: It prints the word Holberton, followed by a new line.
+*Return: 0.
+*/
+int main(void)
+{
+char T[8] = "_putchar";
+int i;
+
+for (i = 0; i < 8; i++)
+	putchar(T[i]);
+putchar('\n');
+return (0);
+}
